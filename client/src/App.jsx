944af64a48459e704bed8289229364b59d278a08
martinvega2003 <josemartinvt2003@gmail.com>
2025-04-09 15:03:59 -0300

    Set up and Test dark mode theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeModeProvider } from './context/ThemeModeContext'
+import Button from './components/Button'
 
 const App = () => {
+
   return (
-    <div className='text-5xl text-amber-300'>App</div>
+    <ThemeModeProvider>
+      <div className='text-5xl text-amber-300 dark:text-amber-700 dark:bg-gray-800'>App</div>
+      <Button />
+    </ThemeModeProvider>
   )
 }
 
