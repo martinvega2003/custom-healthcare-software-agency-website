3582ff76081f6be6d546df0b3ebfd7e24af513a0
martinvega2003 <josemartinvt2003@gmail.com>
2025-04-10 10:07:21 -0300

    Update Button Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { ThemeModeProvider } from './context/ThemeModeContext'
 import Button from './components/Button'
 
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <ThemeModeProvider>
       <div className='text-5xl text-amber-300 dark:text-amber-700 dark:bg-gray-800'>App</div>
-      <Button />
+      <Button className='m-2' onClick={() => console.log('Button clicked!')} variant='link'>Click Me</Button>
     </ThemeModeProvider>
   )
 }
--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,13 +1,30 @@
-import React, { useContext } from 'react'
-import { ThemeModeContext } from '../context/ThemeModeContext'
+import React from "react";
 
-const Button = () => {
-
-  const { darkMode, setDarkMode } = useContext(ThemeModeContext);
+const Button = ({ children, onClick, type = "button", variant = "primary", className }) => {
+  const baseClasses = "px-4 py-2 rounded-lg font-semibold transition duration-200 cursor-pointer " + className;
+  const variants = {
+    primary: "bg-blue-500 text-white hover:bg-blue-800",
+    danger: "bg-red-500 text-white hover:bg-red-800",
+    success: "bg-green-500 text-white hover:bg-green-800",
+    warning: "bg-yellow-500 text-white hover:bg-yellow-800",
+    info: "bg-blue-300 text-white hover:bg-blue-500",
+    light: "bg-gray-200 text-gray-800 hover:bg-gray-300",
+    dark: "bg-gray-800 text-white hover:bg-gray-700",
+    link: "text-blue-500 hover:text-blue-800 underline",
+    ghost: "bg-transparent text-blue-500 hover:bg-blue-500 hover:text-white",
+    outline: "border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white",
+    none: '',
+  };
 
   return (
-      <button onClick={() => setDarkMode(!darkMode)}>Button</button>
-  )
-}
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${baseClasses} ${variants[variant]}`}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
