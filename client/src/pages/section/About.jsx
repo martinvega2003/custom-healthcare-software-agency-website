6ef7a6e0a517e856f50e6c797a68141ecd0a6ebd
martinvega2003 <josemartinvt2003@gmail.com>
2025-04-19 12:29:15 -0300

    Add dark color design to the About Section component

--- client/src/pages/section/About.jsx
+++ client/src/pages/section/About.jsx
@@ -4,14 +4,14 @@ import image from "../../images/about.png";
 const About = () => {
   
   return (
-    <section id='about' className="relative z-20 bg-gray-50 py-16">
+    <section id='about' className="relative z-20 bg-gray-50 dark:bg-gray-900 py-16 transition-colors duration-300">
       <div className="container mx-auto px-6">
         {/* Heading */}
         <div className="text-center mb-12">
-          <h2 className="text-3xl lg:text-5xl font-bold text-blue-600">
+          <h2 className="text-3xl lg:text-5xl font-bold text-blue-600 dark:text-white transition-colors duration-300">
             About Me And You
           </h2>
-          <p className="text-lg text-gray-600 mt-4">
+          <p className="text-lg text-gray-600 dark:text-gray-300 mt-4 transition-colors duration-300">
             Discover my story, values, and how we can work together and help each other.
           </p>
         </div>
@@ -24,8 +24,8 @@ const About = () => {
               alt="Our Clinic"
             />
           </div>
-          <div className="relative bg-blue-500 lg:w-1/2 mt-8 lg:mt-0 p-6 rounded-br-[100px] shadow-2xl group cursor-pointer">
-            <div className="absolute -z-10 -inset-[2px] border-2 border-blue-800 rounded-br-[100px] translate-x-2 translate-y-2 group-hover:translate-0 transition duration-300"></div>
+          <div className="relative bg-blue-500 dark:bg-gray-800 lg:w-1/2 mt-8 lg:mt-0 p-6 rounded-br-[100px] shadow-2xl transition-colors duration-300 group cursor-pointer">
+            <div className="absolute -z-10 -inset-[2px] border-2 border-blue-800 dark:border-gray-700 rounded-br-[100px] translate-x-2 translate-y-2 group-hover:translate-0 transition duration-300"></div>
             <h3 className="text-2xl font-semibold text-white">
               My Story and Values
             </h3>
