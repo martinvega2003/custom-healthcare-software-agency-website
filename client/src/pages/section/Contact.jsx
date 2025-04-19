39691d7d69d920794ece1897638b80b4a62f3617
martinvega2003 <josemartinvt2003@gmail.com>
2025-04-19 13:02:34 -0300

    Add dark color design to the Contact Section component

--- client/src/pages/section/Contact.jsx
+++ client/src/pages/section/Contact.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import bgImageUrl from "../../images/hero.webp";
 import Button from "../../components/Button";
+import { ThemeModeContext } from "../../context/ThemeModeContext";
 
 const Contact = () => {
+
+  const { darkMode } = useContext(ThemeModeContext); // Access dark mode context for button variants
+
   return (
     <section id='contact' className="relative py-16">
+      <div className="absolute inset-0 bg-transparent dark:bg-gray-900 opacity-70 transition-colors duration-300" />
       {/* Background Image Layer */}
       <div
         className="fixed z-0 inset-0 bg-fixed bg-cover bg-center bg-no-repeat opacity-50"
@@ -18,9 +23,9 @@ const Contact = () => {
       {/* Content Layer */}
       <div className="relative z-10 px-6 sm:px-12 md:px-24 flex md:justify-end justify-center">
         {/* Contact Form and Information */}
-        <div className="bg-white shadow-lg rounded-lg p-8">
-          <h2 className="text-4xl font-bold mb-4">Contact</h2>
-          <p className="text-md lg:text-lg xl:text-xl mb-6">
+        <div className="bg-white dark:bg-gray-900 shadow-lg rounded-lg p-8 transition-colors duration-300">
+          <h2 className="text-4xl dark:text-white font-bold mb-4 transition-colors duration-300">Contact</h2>
+          <p className="text-md lg:text-lg xl:text-xl dark:text-gray-300 mb-6 transition-colors duration-300">
             If you have any questions or something to talk about, please don't hestitate to contact.
           </p>
           <form className="space-y-4">
@@ -28,24 +33,24 @@ const Contact = () => {
               <input
                 type="text"
                 placeholder="Name"
-                className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+                className="dark:text-gray-300 placeholder:text-gray-500 border border-gray-300 dark:border-gray-500 rounded-lg px-4 py-2 w-full transition-colors duration-300"
               />
               <input
                 type="email"
                 placeholder="Email"
-                className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+                className="dark:text-gray-300 placeholder:text-gray-500 border border-gray-300 dark:border-gray-500 rounded-lg px-4 py-2 w-full transition-colors duration-300"
               />
               <input
                 type="text"
                 placeholder="Phone Number"
-                className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+                className="dark:text-gray-300 placeholder:text-gray-500 border border-gray-300 dark:border-gray-500 rounded-lg px-4 py-2 w-full transition-colors duration-300"
               />
             </div>
             <textarea
               placeholder="Message"
-              className="border border-gray-300 rounded-lg px-4 py-2 w-full h-32"
+              className="dark:text-gray-300 placeholder:text-gray-500 border border-gray-300 dark:border-gray-500 rounded-lg px-4 py-2 w-full h-32 transition-colors duration-300"
             />
-            <Button type="submit" className="w-full">
+            <Button variant={darkMode ? 'light' : 'primary'} type="submit" className="w-full">
               Submit
             </Button>
           </form>
